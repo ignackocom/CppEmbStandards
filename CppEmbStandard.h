135d314f9176edcba6b78bed219ae2711894cd55
ignackocom <peter.potrok@outlook.sk>
2025-03-18 11:25:57 +0100

    Add files via upload

---
+++ CppEmbStandard.h
@@ -0,0 +1,21 @@
+/******************************************************************************
+* \file      CppEmbStandard.h
+* \version   2025.03.18.01
+* \author    Peter Potrok
+* \copyright Copyright (c) 1994 - 2025
+*            MIT License (see License.txt file)
+* \brief     Cpp Embedded Standard definitions
+* \details
+*
+* \see       https://en.wikipedia.org/wiki/Digital_signal_processing
+*            https://github.com/cpredef/predef
+******************************************************************************/
+
+#ifndef CPPEMBSTANDARD_H
+#define CPPEMBSTANDARD_H		20250318L
+
+
+/* RFU - Reserved for Future Use */
+
+
+#endif /* CPPEMBSTANDARD_H */
